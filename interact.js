9bfbeccb5c41198f89fe2d33352d1a7ed73d9de4
OmarThe <stomatoth@gmail.com>
2021-08-29 22:04:57 -0400

    Third Update

--- interact.js
+++ interact.js
@@ -1,4 +1,5 @@
 function movement(){
+
     //changing the animation to "dead"
     if(placeHolder2 == 1 && player.isTouching(ground1)){
         player.changeAnimation("dead", player_Dead_img)
@@ -13,41 +14,42 @@ function movement(){
         stage = 3
         stageNeed = 3
     
-
+    //allows the player to jump
     }else if(keyIsDown(32) && gameState == "play"  && jumpCounter <  maxJump && player.velocityY > -18.6 && ability2 == 0){
         player.velocityY = -20
         jumpCounter = jumpCounter + 1; 
         jumpStop = 1;
 
-    }else if(keyIsDown(LEFT_ARROW) && gameState == "play"){
-        if(keyIsDown(16)){
-            player.x = player.x-26
-        } else{
-            player.x = player.x-16
-            player.mirrorX(-1)
-            player.changeAnimation("Run")
-            player.setCollider("rectangle", 100,0, 200, 450)
-            jumpStop = 0;
-        }
-    } else if(keyIsDown(RIGHT_ARROW) && gameState == "play"){
-        if(keyIsDown(16)){
-            player.x = player.x+26
-        } else{
-            player.x = player.x+16
-            player.mirrorX(1)
-            player.setCollider("rectangle", -180,0, 200, 450)
-            player.changeAnimation("Run")
-            jumpStop = 0;
-        }
+    //allows the player to walk left
+    }else if(keyIsDown(65) && gameState == "play"){
+        
+        player.x = player.x-16
+        player.mirrorX(-1)
+        player.changeAnimation("Run")
+        player.setCollider("rectangle", 100,0, 200, 450)
+        jumpStop = 0;
+
+    //allows the player to walk right
+    } else if(keyIsDown(68) && gameState == "play"){
+        
+        player.x = player.x+16
+        player.mirrorX(1)
+        player.setCollider("rectangle", -180,0, 200, 450)
+        player.changeAnimation("Run")
+        jumpStop = 0;
+
+    //changes the playes animation to "idle"
     } else{
         player.changeAnimation("Idle")
         jumpStop = 0;
     }
 
+    //changes the players animation to "jump"
       if(gameState == "play"  && !jumpCounter == 0){
         player.changeAnimation("Jump")
     }
 
+    //changes the gameState to "play"
     if(keyDown(ENTER)){
         gameState = "play"
         story.destroy()
@@ -65,17 +67,21 @@ function movement(){
         dinoHead4.velocityX = -7
       }  
 
-    player.velocityY += 1.3
+    //adds gravity to the player
+        player.velocityY += 1.3
 
+    //allows the jump function to work
     if(player.isTouching(ground1) && jumpStop == 0){
         jumpCounter = 0
     }
 
+    //switches stage "1" to stage "2"
     if(player.isTouching(stageEnd1) && placeHolder2 == 0){
         stage = 2
         stageNeed = 2
     }
 
+    //stage "2" start
     if(stage == 2 && stageNeed == 2){
         player.x = 300
         player.y = 400
@@ -90,7 +96,7 @@ function movement(){
         dinoHead4.destroy()
 
         abilityItem1 = createSprite(displayWidth/2,650,30,30)
-        abilityItem1.addAnimation("Rotating", abilityItem1Animation);
+        abilityItem1.addAnimation("Rotating", abilityItem1Img);
         abilityItem1.changeAnimation("Rotating")   
         
         platform1 = createSprite(1468,500,50,20)
@@ -104,7 +110,7 @@ function movement(){
         platform2.scale = 1
 
         platform3 = createSprite(70,300,50,20)
-        platform3inv = createSprite(70,250.25,120,35)
+        platform3inv = createSprite(70,250.25,120,20)
         platform3.addImage(platformImg)
         platform3.scale = 2
 
@@ -115,28 +121,33 @@ function movement(){
         } 
     }
     
+    //stage "2" function
     if(stage == 2){
+
+        //creates "double jump"
         if(player.isTouching(abilityItem1)){
             ability1 = 1
             abilityItem1.destroy()
         }
 
+        //allows the player to have "double jump"
         if (ability1 == 1) {
             maxJump = 2.1
         }
         
-        
+        //allows the player to jump off of platforms
         if(player.isTouching(platform1inv) || player.isTouching(platform2inv) || player.isTouching(platform3inv)){
             jumpCounter = 0
         } 
 
+        //switches stage "2" to stage "3"
         if(player.isTouching(stageEnd2)){
             stage = 3
             stageNeed = 3
         }
     }
     
-    
+    //stage "3" start
     if(stage == 3 && stageNeed == 3){
         player.x = (displayWidth)/2
         player.y = (displayHeight)/2
@@ -165,7 +176,7 @@ function movement(){
 
 
         abilityItem2 = createSprite(displayWidth/2,650,30,30)
-        abilityItem2.addAnimation("Rotating", abilityItem2Animation);
+        abilityItem2.addAnimation("Rotating", abilityItem2Img);
         abilityItem2.changeAnimation("Rotating")
         maxJump = 1
 
@@ -173,24 +184,145 @@ function movement(){
         if(stage == 3 && stageNeed == 3 && player.x == (displayWidth)/2 && player.y == (displayHeight)/2 ){
             stageNeed = 1
         } 
+        
+        //dino minions
+        // Moving Left
+        dino3_1 = createSprite(displayWidth+100,700, 50, 50);
+        dinoHead3_1 = createSprite(dino3_1.x, dino3_1.y-30, 40, 70);
+        // dinoHead3_1.visible = false;
+        dino3_1.scale = 6
+        dino3_1.mirrorX(-1)
+        dino3_1.addAnimation("move", dino1Img)
+        dino3_1.debug = true
+        dino3_1.setCollider("rectangle", 0,0, 10, 10)
+        dino3_1.velocityX = -7
+        //dinoHead3_1.velocityX = -7
+
+        dino3_2 = createSprite(displayWidth+500,700, 50, 50);
+        dinoHead5_2 = createSprite(dino3_2.x, dino3_2.y-30, 40, 70);
+        //dinoHead5_2.visible = false;
+        dino3_2.scale = 6
+        dino3_2.mirrorX(-1)
+        dino3_2.addAnimation("move", dino2Img)
+        dino3_2.debug = true
+        dino3_2.setCollider("rectangle", 0,0, 10, 10)
+        dino3_2.velocityX = -7
+        // dinoHead5_2.velocityX = -7
+
+        dino3_3 = createSprite(displayWidth+900,700, 50, 50);
+        dinoHead5_3 = createSprite(dino3_3.x, dino3_3.y-30, 40, 70);
+        //dinoHead5_3.visible = false;
+        dino3_3.scale = 6
+        dino3_3.mirrorX(-1)
+        dino3_3.addAnimation("move", dino3Img)
+        dino3_3.debug = true
+        dino3_3.setCollider("rectangle", 0,0, 10, 10)
+        dino3_3.velocityX = -7
+        // dinoHead5_3.velocityX = -7
+
+        dino3_4 = createSprite(displayWidth+1300,700, 50, 50);
+        dinoHead5_4 = createSprite(dino3_4.x, dino3_4.y-30, 40, 70);
+        //dinoHead5_4.visible = false;
+        dino3_4.scale = 6
+        dino3_4.mirrorX(-1)
+        dino3_4.addAnimation("move", dino4Img)
+        dino3_4.debug = true
+        dino3_4.setCollider("rectangle", 0,0, 10, 10)
+        dino3_4.velocityX = -7
+        // dinoHead5_4.velocityX = -7
+
+
+        //Moving Right
+        dino3_5 = createSprite(-100,700, 50, 50);
+        dinoHead5 = createSprite(dino3_1.x, dino3_1.y-30, 40, 70);
+        // dinoHead5_5.visible = false;
+        dino3_5.scale = 6
+        dino3_5.mirrorX(1)
+        dino3_5.addAnimation("move", dino1Img)
+        dino3_5.debug = true
+        dino3_5.setCollider("rectangle", 0,0, 10, 10)
+        dino3_5.velocityX = 7
+        // dinoHead5_5.velocityX = 7
+
+        dino3_6 = createSprite(-500,700, 50, 50);
+        dinoHead5_6 = createSprite(dino3_2.x, dino3_2.y-30, 40, 70);
+        //dinoHead5_6.visible = false;
+        dino3_6.scale = 6
+        dino3_6.mirrorX(1)
+        dino3_6.addAnimation("move", dino2Img)
+        dino3_6.debug = true
+        dino3_6.setCollider("rectangle", 0,0, 10, 10)
+        dino3_6.velocityX = 7
+        // dinoHead5_6.velocityX = 7
+
+        dino3_7 = createSprite(-900,700, 50, 50);
+        dinoHead5_7 = createSprite(dino3_3.x, dino3_3.y-30, 40, 70);
+        //dinoHead5_7.visible = false;
+        dino3_7.scale = 6
+        dino3_7.mirrorX(1)
+        dino3_7.addAnimation("move", dino3Img)
+        dino3_7.debug = true
+        dino3_7.setCollider("rectangle", 0,0, 10, 10)
+        dino3_7.velocityX = 7
+        // dinoHead5_7.velocityX = 7
+
+        dino3_8 = createSprite(-1300,700, 50, 50);
+        dinoHead5_8 = createSprite(dino3_8.x, dino3_8.y-30, 40, 70);
+        //dinoHead5_8.visible = false;
+        dino3_8.scale = 6
+        dino3_8.mirrorX(1)
+        dino3_8.addAnimation("move", dino4Img)
+        dino3_8.debug = true
+        dino3_8.setCollider("rectangle", 0,0, 10, 10)
+        dino3_8.velocityX = 7
+        // dinoHead5_8.velocityX = 7
+
+        dino3_9 = createSprite(-1700,700, 50, 50);
+        dinoHead5_9 = createSprite(dino3_9.x, dino3_9.y-30, 40, 70);
+        //dinoHead5_9.visible = false;
+        dino3_9.scale = 6
+        dino3_9.mirrorX(1)
+        dino3_9.addAnimation("move", dino1Img)
+        dino3_9.debug = true
+        dino3_9.setCollider("rectangle", 0,0, 10, 10)
+        dino3_9.velocityX = 7
+        // dinoHead5_9.velocityX = 7
+        
+        dino3_10 = createSprite(-1100,700, 50, 50);
+        dinoHead5_10 = createSprite(dino3_10.x, dino3_10.y-30, 40, 70);
+        //dinoHead5_10.visible = false;
+        dino3_10.scale = 6
+        dino3_10.mirrorX(1)
+        dino3_10.addAnimation("move", dino2Img)
+        dino3_10.debug = true
+        dino3_10.setCollider("rectangle", 0,0, 10, 10)
+        dino3_10.velocityX = 7
+        // dinoHead5_10.velocityX = 7
+
     }
 
+    //stage "3" function
     if(stage == 3){
 
+        //creates "bigger jump"
         if(player.isTouching(abilityItem2)){
             ability2 = 1;
             abilityItem2.destroy()
             player.scale = 0.6
         }
         
+        //allows the player to have "bigger jump"
         if(keyIsDown(32) && jumpCounter <  maxJump && ability2 == 1){
             player.velocityY = -30
             jumpCounter = jumpCounter + 1; 
             jumpStop = 1;
         }
+        
         if(player.isTouching(stagePlatform1inv)||player.isTouching(stagePlatform2inv)){
             jumpCounter = 0
         }
+
+        // dinoHead5_1.x = dino3_1.x
 
         
          
--- sketch.js
+++ sketch.js
@@ -1,17 +1,21 @@
 //character varaibles
 var player, dino1, dino2, dino3, dino4, boss;
+//lvl 3 dinos
+var dino3_1, dino3_2, dino3_3, dino3_4, dino3_5, dino3_6, dino3_7, dino3_8, dino3_9, dino3_10, dino3_11, dino3_12, dino3_13, dino3_14, dino3_15, dino3_16, dino3_17, dino3_18, dino3_19, dino3_20;
+var dinoHead5_1, dinoHead5_2, dinoHead5_3, dinoHead5_4, dinoHead5_5, dinoHead5_6, dinoHead5_7, dinoHead5_8, dinoHead5_9, dinoHead5_10, dinoHead5_11, dinoHead5_12, dinoHead5_13, dinoHead5_14;
+var dinoHead5_15, dinoHead5_16, dinoHead5_17, dinoHead5_18, dinoHead5_19, dinoHead5_20;
 
 //character aniamations
 var player_img, player_run_img, player_dead_img, player_jump_img, boss;
 
 //hidden dino varaibles
-var dino1Animation, dino2Animation, dino3Animation, dino4Animation;
+var dino1Img, dino2Img, dino3Img, dino4Img;
 var dinoHead1, dinoHead2, dinoHead3, dinoHead4
 
 var bgImage, bgImage2, bgImage3;
-var ground, story, ground1, jumpZone, stageEnd1, stageEnd2;
+var ground, story, ground1, stageEnd1, stageEnd2, boundryUp, boundryLeft, boundryRight;
 var platform1,platform2,platform3,platform1inv,platform2inv,platform3inv;
-var platformImg, platformImg2, stagePlatform, stagePlatform1, stagePlatform2, stagePlatforminv1, stagePlatform2inv, stagePlatformImg
+var platformImg, platformImg2, stagePlatform, stagePlatform1, stagePlatform2, stagePlatforminv1, stagePlatform2inv, stagePlatformImg;
 var gameState = "start";
 
 var placeHolder2 = 0;
@@ -23,7 +27,7 @@ var jumpStop = 0;
 
 var ability1 = 0, ability2 = 0, ability3, ability4;
 var abilityItem1, abilityItem2, abilityItem3, abilityItem4;
-var abilityItem1Animation, abilityItem2Animation, abilityItem3Animation, abilityItem4Animation;
+var abilityItem1Img, abilityItem2Img, abilityItem3Img, abilityItem4Img;
 
 function preload(){
   bgImage = loadImage("backgrounds/Main-Menu.jpg");
@@ -52,40 +56,40 @@ function preload(){
     "./flatboy/png/Jump10.png","./flatboy/png/Jump11.png","./flatboy/png/Jump12.png","./flatboy/png/Jump13.png","./flatboy/png/Jump14.png",
     "./flatboy/png/Jump15.png")
 
-  dino1Animation = loadAnimation("./Mini-Dinos/DinoSprites_doux-0/0.png","./Mini-Dinos/DinoSprites_doux-0/1.png","./Mini-Dinos/DinoSprites_doux-0/2.png",
+  dino1Img = loadAnimation("./Mini-Dinos/DinoSprites_doux-0/0.png","./Mini-Dinos/DinoSprites_doux-0/1.png","./Mini-Dinos/DinoSprites_doux-0/2.png",
     "./Mini-Dinos/DinoSprites_doux-0/3.png","./Mini-Dinos/DinoSprites_doux-0/4.png","./Mini-Dinos/DinoSprites_doux-0/5.png","./Mini-Dinos/DinoSprites_doux-0/6.png",
     "./Mini-Dinos/DinoSprites_doux-0/7.png","./Mini-Dinos/DinoSprites_doux-0/8.png","./Mini-Dinos/DinoSprites_doux-0/9.png",
     "./Mini-Dinos/DinoSprites_doux-0/10.png","./Mini-Dinos/DinoSprites_doux-0/11.png","./Mini-Dinos/DinoSprites_doux-0/12.png","./Mini-Dinos/DinoSprites_doux-0/13.png",
     "./Mini-Dinos/DinoSprites_doux-0/14.png")  
 
-  dino2Animation = loadAnimation("./Mini-Dinos/DinoSprites_mort-0/0.png","./Mini-Dinos/DinoSprites_mort-0/1.png","./Mini-Dinos/DinoSprites_mort-0/2.png",
+  dino2Img = loadAnimation("./Mini-Dinos/DinoSprites_mort-0/0.png","./Mini-Dinos/DinoSprites_mort-0/1.png","./Mini-Dinos/DinoSprites_mort-0/2.png",
     "./Mini-Dinos/DinoSprites_mort-0/3.png","./Mini-Dinos/DinoSprites_mort-0/4.png","./Mini-Dinos/DinoSprites_mort-0/5.png","./Mini-Dinos/DinoSprites_mort-0/6.png",
     "./Mini-Dinos/DinoSprites_mort-0/7.png","./Mini-Dinos/DinoSprites_mort-0/8.png","./Mini-Dinos/DinoSprites_mort-0/9.png",
     "./Mini-Dinos/DinoSprites_mort-0/10.png","./Mini-Dinos/DinoSprites_mort-0/11.png","./Mini-Dinos/DinoSprites_mort-0/12.png","./Mini-Dinos/DinoSprites_mort-0/13.png",
     "./Mini-Dinos/DinoSprites_mort-0/14.png") 
 
-  dino3Animation = loadAnimation("./Mini-Dinos/DinoSprites_tard-0/0.png","./Mini-Dinos/DinoSprites_tard-0/1.png","./Mini-Dinos/DinoSprites_tard-0/2.png",
+  dino3Img = loadAnimation("./Mini-Dinos/DinoSprites_tard-0/0.png","./Mini-Dinos/DinoSprites_tard-0/1.png","./Mini-Dinos/DinoSprites_tard-0/2.png",
     "./Mini-Dinos/DinoSprites_tard-0/3.png","./Mini-Dinos/DinoSprites_tard-0/4.png","./Mini-Dinos/DinoSprites_tard-0/5.png","./Mini-Dinos/DinoSprites_tard-0/6.png",
     "./Mini-Dinos/DinoSprites_tard-0/7.png","./Mini-Dinos/DinoSprites_tard-0/8.png","./Mini-Dinos/DinoSprites_tard-0/9.png",
     "./Mini-Dinos/DinoSprites_tard-0/10.png","./Mini-Dinos/DinoSprites_tard-0/11.png","./Mini-Dinos/DinoSprites_tard-0/12.png","./Mini-Dinos/DinoSprites_tard-0/13.png",
     "./Mini-Dinos/DinoSprites_tard-0/14.png") 
 
-  dino4Animation = loadAnimation("./Mini-Dinos/DinoSprites_vita-0/0.png","./Mini-Dinos/DinoSprites_vita-0/1.png","./Mini-Dinos/DinoSprites_vita-0/2.png",
+  dino4Img = loadAnimation("./Mini-Dinos/DinoSprites_vita-0/0.png","./Mini-Dinos/DinoSprites_vita-0/1.png","./Mini-Dinos/DinoSprites_vita-0/2.png",
     "./Mini-Dinos/DinoSprites_vita-0/3.png","./Mini-Dinos/DinoSprites_vita-0/4.png","./Mini-Dinos/DinoSprites_vita-0/5.png","./Mini-Dinos/DinoSprites_vita-0/6.png",
     "./Mini-Dinos/DinoSprites_vita-0/7.png","./Mini-Dinos/DinoSprites_vita-0/8.png","./Mini-Dinos/DinoSprites_vita-0/9.png",
     "./Mini-Dinos/DinoSprites_vita-0/10.png","./Mini-Dinos/DinoSprites_vita-0/11.png","./Mini-Dinos/DinoSprites_vita-0/12.png","./Mini-Dinos/DinoSprites_vita-0/13.png",
     "./Mini-Dinos/DinoSprites_vita-0/14.png")   
 
-  abilityItem1Animation = loadAnimation("./Coins/Blue/blueCoin1.png","./Coins/Blue/blueCoin2.png","./Coins/Blue/blueCoin3.png",
+  abilityItem1Img = loadAnimation("./Coins/Blue/blueCoin1.png","./Coins/Blue/blueCoin2.png","./Coins/Blue/blueCoin3.png",
     "./Coins/Blue/blueCoin4.png","./Coins/Blue/blueCoin5.png","./Coins/Blue/blueCoin6.png")
 
-  abilityItem2Animation = loadAnimation("./Coins/Bronze/bronzeCoin1.png","./Coins/Bronze/bronzeCoin2.png","./Coins/Bronze/bronzeCoin3.png",
+  abilityItem2Img = loadAnimation("./Coins/Bronze/bronzeCoin1.png","./Coins/Bronze/bronzeCoin2.png","./Coins/Bronze/bronzeCoin3.png",
     "./Coins/Bronze/bronzeCoin4.png","./Coins/Bronze/bronzeCoin5.png","./Coins/Bronze/bronzeCoin6.png")
 
-  abilityItem3Animation = loadAnimation("./Coins/Gold/goldCoin1.png","./Coins/Gold/goldCoin2.png","./Coins/Gold/goldCoin3.png",
+  abilityItem3Img = loadAnimation("./Coins/Gold/goldCoin1.png","./Coins/Gold/goldCoin2.png","./Coins/Gold/goldCoin3.png",
     "./Coins/Gold/goldCoin4.png","./Coins/Gold/goldCoin5.png","./Coins/Gold/goldCoin6.png")
 
-  abilityItem4Animation = loadAnimation("./Coins/Silver/silverCoin1.png","./Coins/Silver/silverCoin2.png","./Coins/Silver/silverCoin3.png",
+  abilityItem4Img= loadAnimation("./Coins/Silver/silverCoin1.png","./Coins/Silver/silverCoin2.png","./Coins/Silver/silverCoin3.png",
     "./Coins/Silver/silverCoin4.png","./Coins/Silver/silverCoin5.png","./Coins/Silver/silverCoin6.png")
 
 }
@@ -101,7 +105,7 @@ function setup() {
   // dinoHead1.visible = false;
   dino1.scale = 6
   dino1.mirrorX(-1)
-  dino1.addAnimation("move", dino1Animation)
+  dino1.addAnimation("move", dino1Img)
   dino1.debug = true
   dino1.setCollider("rectangle", 0,0, 10, 10)
 
@@ -110,7 +114,7 @@ function setup() {
   //dinoHead2.visible = false;
   dino2.scale = 6
   dino2.mirrorX(-1)
-  dino2.addAnimation("move", dino2Animation)
+  dino2.addAnimation("move", dino2Img)
   dino2.debug = true
   dino2.setCollider("rectangle", 0,0, 10, 10)
 
@@ -119,7 +123,7 @@ function setup() {
   //dinoHead3.visible = false;
   dino3.scale = 6
   dino3.mirrorX(-1)
-  dino3.addAnimation("move", dino3Animation)
+  dino3.addAnimation("move", dino3Img)
   dino3.debug = true
   dino3.setCollider("rectangle", 0,0, 10, 10)
 
@@ -128,23 +132,26 @@ function setup() {
   //dinoHead4.visible = false;
   dino4.scale = 6
   dino4.mirrorX(-1)
-  dino4.addAnimation("move", dino4Animation)
+  dino4.addAnimation("move", dino4Img)
   dino4.debug = true
   dino4.setCollider("rectangle", 0,0, 10, 10)
   
-  
-
   //technicall sprites
   ground = createSprite((displayWidth-70)/2,displayHeight-320, displayWidth*2, 30)
   ground1 = createSprite((displayWidth-70)/2,displayHeight-320.00000000000009, displayWidth, 30)
-  jumpZone = createSprite((displayWidth-70)/2,displayHeight-450, displayWidth, 150)
   stageEnd1 = createSprite(displayWidth-100,displayHeight-450)
+
+  boundryLeft = createSprite((displayWidth-displayWidth)-250,displayHeight/2,500,displayHeight)
+  boundryRight = createSprite(displayWidth+250,displayHeight/2,500,displayHeight)
+  boundryUp = createSprite(displayWidth/2,-400,displayWidth,500)
+
+  
   story = createSprite((displayWidth-70)/2,(displayHeight-180)/2, 600, 600)
-  ground.visible = false
-  //ground1.visible = false
-  jumpZone.visible = false
-  stageEnd1.visible = true
   story.shapeColor = "black"
+  //ground.visible = true
+  //ground1.visible = true
+  //stageEnd1.visible = true
+
 
   //player 
   player = createSprite(300, 400, 50, 50);
@@ -195,6 +202,9 @@ function Boss(){
 
 function collide(){
   player.collide(ground)
+  player.collide(boundryLeft)
+  player.collide(boundryRight)
+  player.collide(boundryUp)
 
   if(stage == 2){
     player.collide(platform1)
